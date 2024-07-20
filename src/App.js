f6d3815505341882dca39ef2ee0d538c44a5bc55
brandon chase <brandonchase93@gmail.com>
2024-07-20 16:22:34 -0600

    estup router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import Home from "./pages/Home"
+import ContactUs from "./pages/ContactUs"
+import About from "./pages/About"
 
 function App() {
   return (
-    <div className="App">
-      Recipes Project
+    <div className="app">
+      <BrowserRouter>
+        <Routes >
+          <Route exact path="/" element={ <Home />} />
+          <Route exact path="/about" element={ <About />} />
+          <Route exact path="/contact-us" element={ <ContactUs />} />
+           
+        </Routes >
+      </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return(
+        <div>Home Page
+            <ul>
+                <li >
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
